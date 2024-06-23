9773b609ef0aba544ea4f2859866080fc3094e58
Adroaldo <hygino@msn.com>
2024-06-23 08:58:24 -0300

    Filtrando elementos com a tag audio

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado!');
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -9,14 +17,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
+    const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function () {
